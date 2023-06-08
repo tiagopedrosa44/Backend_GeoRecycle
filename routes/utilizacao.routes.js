34964dd70ccd018a90618e60c2de890e9ef748aa
Fabio <fabionferreira15@gmail.com>
2023-06-08 15:57:59 +0100

    .

--- routes/utilizacao.routes.js
+++ routes/utilizacao.routes.js
@@ -25,7 +25,7 @@ router.use((req, res, next) => {
 const multer = require("multer");
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/temp");
+    cb(null, "uploads");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);
