d587090a17595c4a026220d1939ee0577f093b0b
Fabio <fabionferreira15@gmail.com>
2023-06-08 17:18:25 +0100

    .

--- controllers/utilizacao.controller.js
+++ controllers/utilizacao.controller.js
@@ -43,7 +43,7 @@ exports.registarUtilizacao = async (req, res) => {
 
     res.status(200).json({
       success: true,
-      msg: `Utilização registada com sucesso.${utilizacao_image}`,
+      msg: `Utilização registada com sucesso.${req.file}`,
       img: utilizacao_image,
     });
   } catch (err) {
